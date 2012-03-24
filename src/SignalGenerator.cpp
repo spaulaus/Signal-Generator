8b3773d141b230d8195be637e30262388cba79d1
S.V. Paulauskas <stanpaulauskas@gmail.com>
2012-03-24 01:34:06 -0400

    Updated headers, added doxygen comment.
    Doxyfile:
    Configuration to generate the documentation. Only creates latex files.
    
    TODO:
    Ummm.....
    
    SignalGenerator.hpp/.cpp:
    Updated headers.  Input data files should have three columns x,y,yerr.
    
    pictures.dox:
    Adds the pictures to the manual.
    
    sig.cpp
    An example file for the class.

--- src/SignalGenerator.cpp
+++ src/SignalGenerator.cpp
@@ -1,9 +1,9 @@
-/** \file SignalGenerator.cpp
- * \brief Signal Generator
- * This is a class to generate different types of signals.  
- * You can also add noise to the signal. 
- * Written : S.V. Paulauskas - 15 March 2012
+/* 
+ * Copyright S.V. Paulauskas 2012 
+ * stanpaulauskas@gmail.com
+ * Distributed under GNU General Public Liscence v3
  */
+
 #include <fstream>
 #include <iostream>
 
@@ -15,56 +15,91 @@ using namespace std;
 
 //********** GenerateSignal **********
 void SignalGenerator::GenerateSignal(void) {
-   if (type_ == "cosine")
-      mu_ += M_PI*0.5;
-   
-   for(double i = 0; i < length_; i += res_) {
-      if(type_ == "sine") {
-	 signal_.push_back(SineWave(i));
-      } else if(type_ == "cosine") { 
-	 signal_.push_back(SineWave(i));
-      } else if (type_ == "gaussian") {
-	 signal_.push_back(Gaussian(i));
-      }else if (type_ == "nonstationarysine") {
-	 signal_.push_back(NonStationarySine(i));
-      } else if (type_ == "square") {
-	 signal_.push_back(SquareWave(i));
-      } else if (type_ == "sawtooth") {
-	 signal_.push_back(SawtoothWave(i));
-      } else if (type_ == "triangle") {
-	 signal_.push_back(2.*fabs(SawtoothWave(i)));
-      }
+   if(type_ == "custom") {
+      CustomFunction();
+   }else if(typeA_ != "") {
+      FillVector(signal_, type_);
+     FillVector(signalA_, typeA_);
+      CompositeFunction();
+   }else {
+      FillVector(signal_, type_);
    }
-
+      
    if(hasNoise_) 
       for(unsigned int i = 0; i < signal_.size(); i++) 
 	 signal_[i] += noiseAmp_*Noise();
 }
 
+
 //********** CompositeFunction **********
+void SignalGenerator::CompositeFunction(void) {
+   for(unsigned int i = 0; i < signal_.size(); i++) {
+      signal_[i] += signalA_[i];
+   }
+}
+
 
 //********** CustomFunction **********
 void SignalGenerator::CustomFunction(void) {
-   ifstream readData("dataFiles/realData.dat");
-   if(readData.is_open()) {
-      while(readData.good()) {
-	 double junk0, junk1, junk2;
-	 readData >> junk0 >> junk1 >> junk2;
-	 signal_.push_back(junk1);
+   double junk0, junk1, junk2;
+   if(fileName_ == "") {
+      cout << endl << "A file name must be specified to use a custom function."
+	   << endl << "This is a fatal error. Exiting." << endl;
+      exit(2);
+   }else {
+      ifstream readData("fileName_.c_str()");
+      if(readData.is_open()) {
+	 while(readData.good()) {
+	    readData >> junk0 >> junk1 >> junk2;
+	    signal_.push_back(junk0);
+	    signal_.push_back(junk1);
+	 }
+	 readData.close();
       }
-      readData.close();
    }
 }
+
+
+//********** Fill Vector **********
+void SignalGenerator::FillVector(vector<double> &vec, 
+				 const string &type) {
+   if(type == "cosine")
+      mu_ += M_PI*0.5;
+   
+   for(double i = 0; i < length_; i += res_) {
+      if(type == "sine") {
+    	 vec.push_back(SineWave(i));
+      } else if(type == "cosine") { 
+    	 vec.push_back(SineWave(i));
+      } else if (type == "gaussian") {
+    	 vec.push_back(Gaussian(i));
+      }else if (type == "nonstationarysine") {
+    	 vec.push_back(NonStationarySine(i));
+      } else if (type == "square") {
+    	 vec.push_back(SquareWave(i));
+      } else if (type == "sawtooth") {
+    	 vec.push_back(SawtoothWave(i));
+      } else if (type == "triangle") {
+    	 vec.push_back(2.*fabs(SawtoothWave(i)));
+      }
+   }
+}   
+
+
 //********** Gaussian **********
 double SignalGenerator::Gaussian(const double &t) {
    double coeff = amp_/(sigma_*sqrt(2*M_PI));
    double exponent = -pow((t-mu_)/sigma_,2)*0.5;
    return( coeff * exp(exponent) );
 }
+
+
 //********** Noise **********
 double SignalGenerator::Noise(void) {
    return ( (rand() / double(RAND_MAX))*2 -1 );
 }
+
+
 //********** NonStationarySine **********
 double SignalGenerator::NonStationarySine(const double &t) {
    double t1 = sigma_/6., t2 = 4.*sigma_/6.;       
@@ -78,18 +113,26 @@ double SignalGenerator::NonStationarySine(const double &t) {
    else
       return(0.0);
 }
+
+
 //********** SawtoothWave **********
 double SignalGenerator::SawtoothWave(const double &t) {
    return (amp_ * (t/period_ - floor(t/period_ + 0.5)));
 }
+
+
 //********** Sign **********
 double SignalGenerator::Sign(const double &t) {
    return ( (t<0) ? -1 : (t>0) );
 }
+
+
 //********** SineWave **********
 double SignalGenerator::SineWave(const double &t) {
    return( amp_ * sin(period_*t + mu_) );
 }
+
+
 //********** SquareWave **********
 double SignalGenerator::SquareWave(const double &t) {
    return ( amp_ * Sign(SineWave(t)) );
--- src/sig.cpp
+++ src/sig.cpp
@@ -1,34 +1,49 @@
+/* 
+ * Copyright S.V. Paulauskas 2012
+ * stanpaulauskas@gmail.com
+ * Distributed under GNU General Public Licence v3
+ */
+
+/** \example sig.cpp
+ * \brief A simple example.
+ * 
+ * The example generates a simple function, a compound function, 
+ * and finally a custom function.  It will then output these 
+ * functions into separate data files.  
+ */
 #include <iostream>
-
-#include <cmath>
-
 #include "SignalGenerator.hpp"
 
 using namespace std;
 
+/// The main function, to set/get the different stuff for the example.
 int main(int argc, char* argv[])
 {
    string signalType = argv[1];
+   string fileName;
+   if(argc == 3)
+      fileName = argv[2];
+
    double res = 0.01;
    
-   SignalGenerator gen;
+   SignalGenerator sig;
 
    //Set Signal Information
-   gen.SetSignalLength(10);
-   gen.SetSignalResolution(res);
-   gen.SetPeriod(1.);
-   gen.SetAmplitude(5.);
-   gen.SetSigma(1.);
-   gen.SetDelay(0.);
-   gen.SetSignalType(signalType);
-  
+   sig.SetSignalLength(10);
+   sig.SetSignalResolution(res);
+   sig.SetPeriod(1.);
+   sig.SetAmplitude(5.);
+   sig.SetSigma(1.);
+   sig.SetDelay(5.);
+   sig.SetSignalType(signalType);
+
    //Set Noise Information
-   gen.SetNoise(true);
-   gen.SetNoiseAmplitude(0.5);
+   sig.SetNoise(true);
+   sig.SetNoiseAmplitude(0.5);
 
    //Generate and Get Signal
-   gen.GenerateSignal();
-   vector<double> *signal = gen.GetSignal();
+   sig.GenerateSignal();
+   vector<double> *signal = sig.GetSignal();
 
    for(unsigned int i = 0; i < signal->size(); i++)
       cout << i*res << " " << signal->at(i) << endl;
