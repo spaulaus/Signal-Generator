20e90f9de22a2a0f386da912f704bb38b0de66be
S.V. Paulauskas <stanpaulauskas@gmail.com>
2014-05-19 11:51:48 -0400

    Adding in the GNU GPL license

--- src/SignalGenerator.cpp
+++ src/SignalGenerator.cpp
@@ -1,9 +1,19 @@
-/* 
- * Copyright S.V. Paulauskas 2012 
- * stanpaulauskas@gmail.com
- * Distributed under GNU General Public Liscence v3
- */
-
+/***************************************************************************
+  *  Copyright S. V. Paulauskas 2012-2014                                  *
+  *                                                                        *
+  *  This program is free software: you can redistribute it and/or modify  *
+  *  it under the terms of the GNU General Public License as published by  *
+  *  the Free Software Foundation, version 3.0 License.                    *
+  *                                                                        *
+  *  This program is distributed in the hope that it will be useful,       *
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+  *  GNU General Public License for more details.                          *
+  *                                                                        *
+  *  You should have received a copy of the GNU General Public License     *
+  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+  **************************************************************************
+*/
 #include <fstream>
 #include <iostream>
 #include <limits>
